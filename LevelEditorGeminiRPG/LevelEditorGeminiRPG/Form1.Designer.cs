111a6e4c9af536b1a2823f6939824741883e2a5e
James Hulse <emolyptic@gmail.com>
2013-08-10 15:35:14 -0600

    Load Tiles.

--- LevelEditorGeminiRPG/LevelEditorGeminiRPG/Form1.Designer.cs
+++ LevelEditorGeminiRPG/LevelEditorGeminiRPG/Form1.Designer.cs
@@ -28,12 +28,139 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+			this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.loadTilesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+			this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.menuStrip1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// menuStrip1
+			// 
+			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip1.Name = "menuStrip1";
+			this.menuStrip1.Size = new System.Drawing.Size(902, 24);
+			this.menuStrip1.TabIndex = 0;
+			this.menuStrip1.Text = "menuStrip1";
+			// 
+			// fileToolStripMenuItem
+			// 
+			this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.saveToolStripMenuItem,
+            this.loadTilesToolStripMenuItem,
+            this.exitToolStripMenuItem});
+			this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+			this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+			this.fileToolStripMenuItem.Text = "File";
+			// 
+			// saveToolStripMenuItem
+			// 
+			this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+			this.saveToolStripMenuItem.Size = new System.Drawing.Size(127, 22);
+			this.saveToolStripMenuItem.Text = "Save";
+			this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+			// 
+			// loadTilesToolStripMenuItem
+			// 
+			this.loadTilesToolStripMenuItem.Name = "loadTilesToolStripMenuItem";
+			this.loadTilesToolStripMenuItem.Size = new System.Drawing.Size(127, 22);
+			this.loadTilesToolStripMenuItem.Text = "Load Tiles";
+			this.loadTilesToolStripMenuItem.Click += new System.EventHandler(this.loadTilesToolStripMenuItem_Click);
+			// 
+			// exitToolStripMenuItem
+			// 
+			this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+			this.exitToolStripMenuItem.Size = new System.Drawing.Size(127, 22);
+			this.exitToolStripMenuItem.Text = "Exit";
+			this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+			// 
+			// flowLayoutPanel1
+			// 
+			this.flowLayoutPanel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 163);
+			this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+			this.flowLayoutPanel1.Size = new System.Drawing.Size(239, 246);
+			this.flowLayoutPanel1.TabIndex = 2;
+			// 
+			// flowLayoutPanel2
+			// 
+			this.flowLayoutPanel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.flowLayoutPanel2.Location = new System.Drawing.Point(245, 27);
+			this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+			this.flowLayoutPanel2.Size = new System.Drawing.Size(657, 382);
+			this.flowLayoutPanel2.TabIndex = 3;
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.pictureBox1.Location = new System.Drawing.Point(128, 28);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(98, 105);
+			this.pictureBox1.TabIndex = 4;
+			this.pictureBox1.TabStop = false;
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(22, 62);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.ReadOnly = true;
+			this.textBox1.Size = new System.Drawing.Size(100, 20);
+			this.textBox1.TabIndex = 5;
+			this.textBox1.Text = "ACTIVE TILE";
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(40, 137);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.ReadOnly = true;
+			this.textBox2.Size = new System.Drawing.Size(100, 20);
+			this.textBox2.TabIndex = 6;
+			this.textBox2.Text = "LOADED TILES";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(902, 410);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.flowLayoutPanel2);
+			this.Controls.Add(this.flowLayoutPanel1);
+			this.Controls.Add(this.menuStrip1);
+			this.MainMenuStrip = this.menuStrip1;
+			this.Name = "Form1";
+			this.ShowInTaskbar = false;
 			this.Text = "Form1";
+			this.menuStrip1.ResumeLayout(false);
+			this.menuStrip1.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.MenuStrip menuStrip1;
+		private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem loadTilesToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+		private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+		private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.TextBox textBox2;
 	}
 }
 
--- LevelEditorGeminiRPG/LevelEditorGeminiRPG/Form1.cs
+++ LevelEditorGeminiRPG/LevelEditorGeminiRPG/Form1.cs
@@ -11,9 +11,98 @@ namespace LevelEditorGeminiRPG
 {
 	public partial class Form1 : Form
 	{
+		List<PictureBox> Tiles;
+		PictureBox[][] Map;
+
 		public Form1()
 		{
 			InitializeComponent();
+			Tiles = new List<PictureBox>();
+			Map = new PictureBox[20][];
+			CreateGrid();
 		}
+
+		private void pictureBox1_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void loadTilesToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			OpenFileDialog d = new OpenFileDialog();
+
+			// allow multiple selection
+			d.Multiselect = true;
+
+			// filter the desired file types
+			d.Filter = "JPG |*.jpg|PNG|*.png|BMP|*.bmp";
+
+			// show the dialog and check if the selection was made
+
+			//this.pictureBox1.
+
+			if (d.ShowDialog() == DialogResult.OK)
+			{
+				foreach (string image in d.FileNames)
+				{
+					// create a new control
+					PictureBox pb = new PictureBox();
+
+					//pb.Tag = tag;
+					//btn.Tag = tag;
+					pb.MouseDown += pictureBox_MouseDown;
+					
+					// assign the image
+					pb.Image = new Bitmap(image);
+
+					//listaImagens.Add(new Bitmap(image));
+
+					// stretch the image
+					pb.SizeMode = PictureBoxSizeMode.StretchImage;
+
+					// set the size of the picture box
+					pb.Height = 32;
+					pb.Width = 32;
+
+					// add the control to the container
+					this.flowLayoutPanel1.Controls.Add(pb);
+					this.flowLayoutPanel1.AutoScroll = true;
+					//this.pictureBox1.Autoscroll = true;
+
+					Tiles.Add(pb);
+					//tag++;
+
+				}
+
+			}
+
+
+		}
+
+		private void pictureBox_MouseDown(object sender, EventArgs e)
+		{ 
+			this.pictureBox1.Image = (PictureBox)sender.Image;
+		}
+
+		private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void CreateGrid()
+		{
+
+		}
+
+
+
+
+
+
 	}
 }
